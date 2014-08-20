6838c2dbebe72cfb7039dacb4836b2ab11b9e38d
Jonathan Sudhakar <jonathangizmo@gmail.com>
2014-08-20 14:01:30 -0700

    Initial script

--- src/Jonathansudhakar/Botdetector/Botdetector.php
+++ src/Jonathansudhakar/Botdetector/Botdetector.php
@@ -3,7 +3,16 @@
 class Botdetector {
 
   public static function detect(){
-    return true;
+    return $this->detectBot();
+  }
+
+  private static function detectBot()
+  {
+    $useragent = (Request::server('HTTP_USER_AGENT')==NULL?'':Request::server('HTTP_USER_AGENT'));
+
+    $humans = Config::get('config.humans');
+
+    return in_array($useragent , $humans);
   }
 
 }
